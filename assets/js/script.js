2b5b20b83cc07ef971f536fadb978e35328bc77d
Onur Çetiner <onurcetineer1@gmail.com>
2024-02-28 12:39:20 +0300

    Travel Website - With, HTML - CSS - Javascript Sample
    Kullanılan sistemler:
    Sticky Header
    Grid & Flex
    Masonry Flex Layout

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const preLoader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", ()=> {
+    preLoader.classList.add("remove");
+});
+
+
+
+
+const addEventOnElements = function (elements, eventType, callback){
+    for (let i = 0, len = elements.length; i< len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**
+ * navbar toggle for mobile
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header");
+window.addEventListerner("scroll", function (){
+    header.classList[windows.scrollY > 100 ? "add" : "remove"]("active");
+});
